047eb5d619f01984269f12627d4542dc3c4968c7
Abdul Hafeez <ahafeez@teresol.com>
2025-05-05 17:29:21 +0500

    first commit

---
+++ host/src/App.jsx
@@ -0,0 +1,32 @@
+
+import React, { Suspense } from 'react';
+// Lazy load remote module
+const RemoteComponentWithCentering = React.lazy(() => import('remoteApp/RemoteComponentWithCentering'));
+
+
+function App() {
+
+  return (
+
+    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] grid-rows-[auto_1fr_auto] min-h-screen">
+      {/* Sidebar: hidden on small screens, visible on md and up */}
+      <aside className="hidden md:block row-span-3 bg-gray-100 p-4">Sidebar</aside>
+
+      {/* Header: will always span the full width */}
+      <header className="bg-blue-500 p-4 md:col-start-2">Header</header>
+
+      {/* Main Content: Ensure this takes up the available height and doesn't exceed the screen */}
+      <main className="bg-white p-4 md:col-start-2 flex flex-col h-full overflow-hidden">
+        {/* Add your remote content here */}
+        <Suspense fallback={<div>Loading remote...</div>}>
+           <RemoteComponentWithCentering />
+        </Suspense>
+      </main>
+
+      {/* Footer: will always span the full width */}
+      <footer className="bg-blue-500 p-4 md:col-start-2">Footer</footer>
+    </div>
+  )
+}
+
+export default App
---
+++ remote/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+
+   <div>abc</div>
+
+  )
+}
+
+export default App
